c3f448a9e5391bd09f6bb6d8c095f59fe6d74403
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2021-09-27 18:21:32 -0500

    flatlist exercise

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
 // App.js
 
 import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
 import ColorBox from './components/ColorBox';
+
+const COLORS = [
+  { colorName: 'Base03', hexCode: '#002b36' },
+  { colorName: 'Base02', hexCode: '#073642' },
+  { colorName: 'Base01', hexCode: '#586e75' },
+  { colorName: 'Base00', hexCode: '#657b83' },
+  { colorName: 'Base0', hexCode: '#839496' },
+  { colorName: 'Base1', hexCode: '#93a1a1' },
+  { colorName: 'Base2', hexCode: '#eee8d5' },
+  { colorName: 'Base3', hexCode: '#fdf6e3' },
+  { colorName: 'Yellow', hexCode: '#b58900' },
+  { colorName: 'Orange', hexCode: '#cb4b16' },
+  { colorName: 'Red', hexCode: '#dc322f' },
+  { colorName: 'Magenta', hexCode: '#d33682' },
+  { colorName: 'Violet', hexCode: '#6c71c4' },
+  { colorName: 'Blue', hexCode: '#268bd2' },
+  { colorName: 'Cyan', hexCode: '#2aa198' },
+  { colorName: 'Green', hexCode: '#859900' },
+];
+
+const FOODS = ['Apples', 'Broccoli', 'Cookies', 'Doritos', 'Eclairs'];
 
 const App = () => {
   return (
@@ -16,15 +37,18 @@ const App = () => {
       OR
     <View style={[styles.firstStyle, { backgroundColor: 'teal' }]} />
       */}
-      <View style={styles.header}>
-        <Text style={{ fontWeight: 'bold' }}>
-          Here are some boxes of different colors
-        </Text>
-      </View>
-      <ColorBox hexCode="#2aa198" colorName="Cyan" />
+
+      <FlatList
+        data={COLORS}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <ColorBox hexCode={item.hexCode} colorName={item.colorName} />
+        )}
+      />
+      {/* <ColorBox hexCode="#2aa198" colorName="Cyan" />
       <ColorBox hexCode="#268bd2" colorName="Blue" />
       <ColorBox hexCode="#d33682" colorName="Magenta" />
-      <ColorBox hexCode="#cb4b16" colorName="Orange" />
+      <ColorBox hexCode="#cb4b16" colorName="Orange" /> */}
     </SafeAreaView>
   );
 };
