c10d1958f2e47a9ead063fcbaf8c78b594bd377f
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2021-09-28 17:30:17 -0500

    navigation tutorial

--- App.js
+++ App.js
@@ -1,91 +1,26 @@
 // App.js
 
+import 'react-native-gesture-handler';
 import React from 'react';
-import { Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
-import ColorBox from './components/ColorBox';
 import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/Home';
+import ColorPalette from './screens/ColorPalette';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const COLORS = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb4b16' },
-  { colorName: 'Red', hexCode: '#dc322f' },
-  { colorName: 'Magenta', hexCode: '#d33682' },
-  { colorName: 'Violet', hexCode: '#6c71c4' },
-  { colorName: 'Blue', hexCode: '#268bd2' },
-  { colorName: 'Cyan', hexCode: '#2aa198' },
-  { colorName: 'Green', hexCode: '#859900' },
-];
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <SafeAreaView>
-        {/* // Here's an example of how to use styles without a stylesheet
-      // <View style={{ backgroundColor: 'teal' }} />
-      // First set of brackets tells JSX passing an object, and the second is the actual object */}
-
-        {/* To add multiple styles to an element, can pass an array of StyleSheet objects like:
-      <View style={[styles.firstStyle, styles.secondStyle]} />
-        OR
-      <View style={[styles.firstStyle, { backgroundColor: 'teal' }]} />
-        */}
-
-        <Text style={styles.header}>Here is a list of colors</Text>
-
-        <FlatList
-          data={COLORS}
-          keyExtractor={(item) => item}
-          renderItem={({ item }) => (
-            <ColorBox hexCode={item.hexCode} colorName={item.colorName} />
-          )}
-        />
-      </SafeAreaView>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ColorPalette" component={ColorPalette} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    // backgroundColor: '#2aa198',
-    marginHorizontal: 20,
-    paddingHorizontal: 10,
-    marginVertical: 5,
-    paddingVertical: 10,
-    alignItems: 'center',
-    color: 'white',
-    borderRadius: 5,
-    // justifyContent: 'space-around',
-    // flex: 1,
-    // justifyContent: 'center',
-  },
-  header: {
-    // backgroundColor: '#2aa198',
-    marginHorizontal: 10,
-    paddingHorizontal: 10,
-    marginVertical: 5,
-    paddingVertical: 0,
-    alignItems: 'flex-start',
-    paddingTop: 30,
-    fontWeight: 'bold',
-    // justifyContent: 'space-around',
-    // flex: 1,
-    // justifyContent: 'center',
-  },
-  containerText: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-});
 
 // Cyan: #2aa198
 // Blue: #268bd2
@@ -113,3 +48,22 @@ const styles = StyleSheet.create({
 //     justifyContent: 'center',
 //   },
 // });
+
+// const COLORS = [
+//   { colorName: 'Base03', hexCode: '#002b36' },
+//   { colorName: 'Base02', hexCode: '#073642' },
+//   { colorName: 'Base01', hexCode: '#586e75' },
+//   { colorName: 'Base00', hexCode: '#657b83' },
+//   { colorName: 'Base0', hexCode: '#839496' },
+//   { colorName: 'Base1', hexCode: '#93a1a1' },
+//   { colorName: 'Base2', hexCode: '#eee8d5' },
+//   { colorName: 'Base3', hexCode: '#fdf6e3' },
+//   { colorName: 'Yellow', hexCode: '#b58900' },
+//   { colorName: 'Orange', hexCode: '#cb4b16' },
+//   { colorName: 'Red', hexCode: '#dc322f' },
+//   { colorName: 'Magenta', hexCode: '#d33682' },
+//   { colorName: 'Violet', hexCode: '#6c71c4' },
+//   { colorName: 'Blue', hexCode: '#268bd2' },
+//   { colorName: 'Cyan', hexCode: '#2aa198' },
+//   { colorName: 'Green', hexCode: '#859900' },
+// ];
