aeaf2c93539951b7d786e388810b649bd9684371
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2021-09-28 21:16:54 -0500

    routing exercise step 3

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,50 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const COLOR_PALETTES = [
+  {
+    name: 'Solarized',
+    array: [
+      { colorName: 'Base03', hexCode: '#002b36' },
+      { colorName: 'Base02', hexCode: '#073642' },
+      { colorName: 'Base01', hexCode: '#586e75' },
+      { colorName: 'Base00', hexCode: '#657b83' },
+      { colorName: 'Base0', hexCode: '#839496' },
+      { colorName: 'Base1', hexCode: '#93a1a1' },
+      { colorName: 'Base2', hexCode: '#eee8d5' },
+      { colorName: 'Base3', hexCode: '#fdf6e3' },
+      { colorName: 'Yellow', hexCode: '#b58900' },
+      { colorName: 'Orange', hexCode: '#cb4b16' },
+      { colorName: 'Red', hexCode: '#dc322f' },
+      { colorName: 'Magenta', hexCode: '#d33682' },
+      { colorName: 'Violet', hexCode: '#6c71c4' },
+      { colorName: 'Blue', hexCode: '#268bd2' },
+      { colorName: 'Cyan', hexCode: '#2aa198' },
+      { colorName: 'Green', hexCode: '#859900' },
+    ],
+  },
+  {
+    name: 'Rainbow',
+    array: [
+      { colorName: 'Red', hexCode: '#FF0000' },
+      { colorName: 'Orange', hexCode: '#FF7F00' },
+      { colorName: 'Yellow', hexCode: '#FFFF00' },
+      { colorName: 'Green', hexCode: '#00FF00' },
+      { colorName: 'Violet', hexCode: '#8B00FF' },
+    ],
+  },
+  {
+    name: 'Frontend Masters',
+    array: [
+      { colorName: 'Red', hexCode: '#c02d28' },
+      { colorName: 'Black', hexCode: '#3e3e3e' },
+      { colorName: 'Grey', hexCode: '#8a8a8a' },
+      { colorName: 'White', hexCode: '#ffffff' },
+      { colorName: 'Orange', hexCode: '#e66225' },
+    ],
+  },
+];
 
 const COLORS = [
   { colorName: 'Base03', hexCode: '#002b36' },
@@ -39,18 +83,22 @@ const FRONTEND_MASTERS = [
 
 const Home = ({ navigation }) => {
   return (
-    <View>
-      <TouchableOpacity
-        onPress={() =>
-          navigation.push('ColorPalette', {
-            paletteName: 'Solarized',
-            colors: COLORS,
-          })
-        }
-      >
-        <Text>Solarized</Text>
-      </TouchableOpacity>
-    </View>
+    <FlatList
+      data={COLOR_PALETTES}
+      keyExtractor={(item) => item.name}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          onPress={() =>
+            navigation.push('ColorPalette', {
+              paletteName: item.name,
+              colors: item.array,
+            })
+          }
+        >
+          <Text>{item.name}</Text>
+        </TouchableOpacity>
+      )}
+    />
   );
 };
 
