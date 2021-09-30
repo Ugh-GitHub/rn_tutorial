b121094b72edb57addb0be862d28565c4926ef6e
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2021-09-30 14:29:03 -0500

    added modal

--- App.js
+++ App.js
@@ -1,33 +1,39 @@
-// App.js
-
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Home from './screens/Home';
 import ColorPalette from './screens/ColorPalette';
-import { createStackNavigator } from '@react-navigation/stack';
+import AddNewPaletteModal from './screens/AddNewPaletteModal';
 
-const Stack = createStackNavigator();
-
+const MainStack = createStackNavigator();
+const RootStack = createStackNavigator();
+const MainStackScreen = () => {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen name="Home" component={Home} />
+      <MainStack.Screen
+        name="ColorPalette"
+        component={ColorPalette}
+        options={({ route }) => ({ title: route.params.paletteName })}
+      />
+    </MainStack.Navigator>
+  );
+};
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: 'My home' }}
+      <RootStack.Navigator mode="modal">
+        <RootStack.Screen
+          name="Main"
+          component={MainStackScreen}
+          options={{ headerShown: false }}
         />
-        <Stack.Screen
-          name="ColorPalette"
-          component={ColorPalette}
-          options={({ route }) => ({ title: route.params.paletteName })}
-        />
-      </Stack.Navigator>
+        <RootStack.Screen name="AddNewPalette" component={AddNewPaletteModal} />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 };
-
 export default App;
 
 // Cyan: #2aa198
